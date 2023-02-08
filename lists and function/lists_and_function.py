7ddad5724cbdaa817eb79413096b2b9ad013de6f
Siil777 <118188578+Siil777@users.noreply.github.com>
2023-02-08 14:13:47 +0200

    Update lists_and_function.py

--- lists and function/lists_and_function.py
+++ lists and function/lists_and_function.py
@@ -9,7 +9,7 @@ from module1 import*
 
 while True:
     print(f'-----------------------------------------------------------------------------------------------')
-    print(f'\n0 read from file\n1 input workers and birthday,\n2 save to file.\n3 youngest workers,\n4 average year of birth,\n5 year worker\n6 show pensioners,\n7 show younger or older then certain year\n8 exit')
+    print(f'\n0 read from file\n1 input workers and birthday,\n2 save to file.\n3 youngest workers,\n4 average year of birth,\n5 year worker\n6 show pensioners,\n7 show younger or older then certain year\n8 delete\n9 exit')
     v=input('>>:')
     if v=='0':
         birthday=[]
@@ -39,8 +39,13 @@ while True:
     elif v=='6':
         pensioner(zipped)
     elif v=='7':
-        choice=input("would you like to know who older to whom? (1 или 2)")
+        choice=input("would you like to know who older to whom? (1 Ð¸Ð»Ð¸ 2)")
         amount=input("input year for comparing: ")
         lower_or_bigger(choice,amount,zipped)
-    elif v=='8':
+    elif v=='8': 
+        workers,birthday=delete(input('name:'),workers,birthday) 
+        print(workers)  
+        print(birthday)
+
+    elif v=='9':
         break
